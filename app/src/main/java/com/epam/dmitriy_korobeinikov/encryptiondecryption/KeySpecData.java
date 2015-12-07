563e3ea80f2f97591f4e2531145cca7d1eec7d7e
Dmitriy Korobeinikov <Dmitriy_Korobeinikov@epam.com>
2015-12-07 16:08:53 +0400

    Initial commit

---
+++ app/src/main/java/com/epam/dmitriy_korobeinikov/encryptiondecryption/KeySpecData.java
@@ -0,0 +1,9 @@
+package com.epam.dmitriy_korobeinikov.encryptiondecryption;
+
+/**
+ * Created by Dmitriy_Korobeinikov on 12/7/2015.
+ */
+public class KeySpecData {
+    public String modulus;
+    public String exponent;
+}
